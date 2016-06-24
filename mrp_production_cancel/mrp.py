adc319a95713c57309d63356633887c001160197
Manami Hashi <manami@roomsfor.hk>
2016-06-24 10:30:08 +0000

    [ADD] add modules

---
+++ mrp_production_cancel/mrp.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2012 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info@vauxoo.com
+############################################################################
+#    Coded by: julio (julio@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import osv
+
+import openerp.netsvc as netsvc
+from openerp.tools.translate import _
+
+
+class mrp_production(osv.Model):
+    _inherit = "mrp.production"
+
+    def action_cancel(self, cr, uid, ids, context=None):
+        wf_service = netsvc.LocalService("workflow")
+
+        if context is None:
+            context = {}
+
+        move_obj = self.pool.get('stock.move')
+        for production in self.browse(cr, uid, ids, context=context):
+            if production.picking_id.id:
+                wf_service.trg_validate(
+                    uid, 'stock.picking', production.picking_id.id,
+                    'button_cancel', cr)
+            for move in production.move_lines2:
+                account_move_line_id = self.pool.get('account.move.line').search(
+                    cr, uid, [('stock_move_id', '=', move.id)])
+                if account_move_line_id:
+                    move_obj.action_cancel(cr, uid, [move.id])
+                else:
+                    raise osv.except_osv(
+                        'Warning!',
+                        'Cannot cancel manufacturing order without related journal items!'
+                    )
+            if production.move_created_ids2:
+                for move in production.move_created_ids2:
+                    account_move_line_id2 = self.pool.get('account.move.line').search(
+                        cr, uid, [('stock_move_id', '=', move.id)])
+                    if account_move_line_id2:
+                        move_obj.action_cancel(cr, uid, [move.id])
+                    else:
+                        raise osv.except_osv(
+                            'Warning!',
+                            'Cannot cancel manufacturing order without related journal items!'
+                        )
+        return super(mrp_production, self).action_cancel(cr, uid, ids,
+                                                         context=context)
